7473a528c7aac132e9f5896c2774d6839349d7e1
Gonçalo Barroso <128599430+coltginger@users.noreply.github.com>
2024-04-11 15:49:13 +0100

    drawHeader

---
+++ templates/common.php
@@ -0,0 +1,17 @@
+<?php
+
+function drawHeader(){ ?>
+
+<!DOCTYPE html>
+
+    <body>
+        <header>
+            <h1>
+                <a href="/"> TEMPLATE NAME </a>
+            </h1>
+        </header>
+    </body>
+
+<?php
+}
+?>

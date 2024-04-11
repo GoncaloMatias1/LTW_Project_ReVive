7efae956e05d32f931a2a167ac120b9d1b8a7822
GonçaloMatias <up202108703@up.pt>
2024-04-11 17:50:57 +0100

    update db

--- database/connection.db.php
+++ database/connection.db.php
@@ -2,7 +2,7 @@
   declare(strict_types = 1);
 
   function getDatabaseConnection() : PDO {
-    $db = new PDO('sqlite:' . __DIR__ . '/../database/ltw_project.db');
+    $db = new PDO('sqlite:' . __DIR__ . '/../database/sql/ltw_project.db');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 

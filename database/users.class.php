bee571b9159ce9c8c9429081ba2d8da639fdccd7
coltginger <gnbarroso@gmail.com>
2024-04-22 15:14:20 +0100

    User Class Functions

--- database/users.class.php
+++ database/users.class.php
@@ -27,7 +27,7 @@
             $stmt->execute(array($this->name, $this->username, $this->id));
         }
 
-        static function getUser(PDO $db, string $email, string $password) : ?User {
+        static function getUserLogIn(PDO $db, string $email, string $password) : ?User {
 
             $stmt = $db->prepare ('
             SELECT user_id, name, username, email, is_admin
@@ -48,6 +48,37 @@
             }
             else return null;
         }
-    }
 
+        static function getUser(PDO $db, int $id){
+
+            $stmt = $db->prepare('
+            SELECT user_id, name, username, email, is_admin
+            FROM Users
+            WHERE user_id = ?
+            ');
+
+            $stmt->execute(array($id));
+
+            $user = $stmt->fetch();
+
+            return new User(
+                $user['user_id'],
+                $user['name'],
+                $user['username'],
+                $user['email'],
+                $user['is_admin']                
+            );
+        }
+
+        static function insertUser(PDO $db, string $name, string $username, string $email, string $password, bool $is_admin = false): bool{
+            $stmt = $db->prepare('
+                INSERT INTO Users (name, username, email, password, is_admin)
+                VALUES (?, ?, ?, ?, ?)
+            ');
+
+            $hashed_password = sha1($password);
+
+            return $stmt->execute([$name, $username, $email, $hashed_password, $is_admin]);
+        }
+    }
 ?>
